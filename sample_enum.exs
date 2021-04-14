b21c7972eb39f8a09f736b618f7c41a64e226655
JorgePadilla <jorgep4dill4@gmail.com>
2021-04-13 22:38:02 -0600

    Guard

--- sample_enum.exs
+++ sample_enum.exs
@@ -2,7 +2,10 @@ defmodule Sample.Enum do
     def first(list) do
         hd(list)
     end
-    
+    def first_guard(list) when length(list) ==0, do: nil
     def first_match([head | _]), do: head
     def first_match([]), do: nil
+    def add(list, val \\ 0) do
+        [val | list]
+    end
 end
