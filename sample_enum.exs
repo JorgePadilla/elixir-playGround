0d4cf4dda28dd4496c61fba2d73b71a09068d4ab
JorgePadilla <jorgep4dill4@gmail.com>
2021-04-15 10:43:57 -0600

    recursion

--- sample_enum.exs
+++ sample_enum.exs
@@ -2,14 +2,25 @@ defmodule Sample.Enum do
     def first(list) do
         hd(list)
     end
-    def first_guard(list) when length(list) ==0, do: nil
+
+    def first_guard(list) when length(list) == 0, do: nil
+
     def first_match([head | _]), do: head
+
     def first_match([]), do: nil
+
     def add(list, val \\ 0) do
     trace(val)
         [val | list]
     end
 
+
+    def map([], _) ,do: []
+
+    def map([hd | tl], f) do
+        [f.(hd) | map(tl, f)]
+    end
+        
     defp trace(string) do
         IO.puts("The value passed in was #{string}")
     end
